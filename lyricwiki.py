c478c4520b9dc01715fda66e8dc35d0884eafcd3
Jannis Andrija Schnitzer <jannis@myrkur.de>
2010-02-14 01:08:51 +0100

    Added docstrings

--- lyricwiki.py
+++ lyricwiki.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+"""
+lyricwiki.py -- get lyrics from lyrics.wikia.com
+usage: lyricwiki.py "Artist" "Song"
+"""
+
 from sys import argv
 from restkit import Resource
 import html5lib
@@ -8,6 +13,7 @@ import simplejson
 parser = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("lxml"))
 
 def url_from_api(artist, song):
+    """Get URL to lyrics article from lyrics.wikia.com API"""
     r =  Resource('http://lyrics.wikia.com', headers={'Accept':'application/json'})
     json = r.get('/api.php', fmt='json', artist=artist, song=song)
     json = json[6:].replace("'", '"') # replace needed because wikia doesn't provide us with valid JSON. [6:] needed because it says "song = " first.
@@ -15,9 +21,11 @@ def url_from_api(artist, song):
     return d['url']
     
 def artist_song_from_api_url(url):
+    """Get wiki-compatible artist, song tuple from URL retrieved from API"""
     return tuple(url[len('http://lyrics.wikia.com/'):].split(':'))
 
 def edit_url_from(url):
+    """Get URL to "edit this page" from a given lyrics page"""
     source = Resource(url).get()
     document = parser.parse(source)
     
@@ -25,6 +33,7 @@ def edit_url_from(url):
     return ''.join(['http://lyrics.wikia.com', dict(edit_link.items())['href']])
     
 def lyrics_from(base_url, *arguments, **parameters):
+    """Get lyrics string from an edit page"""
     edit_source = Resource(base_url).get(*arguments, **parameters)
     edit_document = parser.parse(edit_source)
     
@@ -34,6 +43,7 @@ def lyrics_from(base_url, *arguments, **parameters):
     return wiki_source[start:end].strip()
 
 def lyrics(artist, song):
+    """Get lyrics using the API and an edit page"""
     api_url = url_from_api(artist, song)
     artist_song = artist_song_from_api_url(api_url)
     return lyrics_from('http://lyrics.wikia.com/', 'index.php', title='{0}:{1}'.format(*artist_song), action='edit')
