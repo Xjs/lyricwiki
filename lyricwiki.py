edf5485027902c1679357a3c3eb7d63ae3367cf8
Jannis Andrija Schnitzer <jannis@myrkur.de>
2010-02-17 21:05:44 +0100

    Fixed unicode behavior

--- lyricwiki.py
+++ lyricwiki.py
@@ -5,27 +5,34 @@ usage: lyricwiki.py "Artist" "Song"
 """
 
 import sys
+import unicodedata
 from restkit import Resource
 import html5lib
 from html5lib import treebuilders
 import simplejson
+from urllib import unquote
 
 parser = html5lib.HTMLParser(tree=treebuilders.getTreeBuilder("lxml"))
+charset = 'utf-8' # TODO: get this from HTTP response
+
+def proper_unicode(string):
+    return unicodedata.normalize('NFC', unicode(string, charset))
 
 def url_from_api(artist, song):
     """Get URL to lyrics article from lyrics.wikia.com API"""
     r =  Resource('http://lyrics.wikia.com', headers={'Accept':'application/json'})
-    json = r.get('/api.php', fmt='json', artist=artist, song=song)
+    json = r.get('/api.php', fmt='json', artist=proper_unicode(artist), song=proper_unicode(song))
     json = json[6:].replace("'", '"') # replace needed because wikia doesn't provide us with valid JSON. [6:] needed because it says "song = " first.
     d = simplejson.loads(json)
     if d['lyrics'] == 'Not found':
         raise ValueError("No lyrics for {song} by {artist}".format(song=song, artist=artist))
     else:
-        return d['url']
+        print >>sys.stderr, 'url =', d['url']
+        return unicode(unquote(d['url'].encode(charset)), charset)
     
 def artist_song_from_api_url(url):
     """Get wiki-compatible artist, song tuple from URL retrieved from API"""
-    return tuple(url[len('http://lyrics.wikia.com/'):].split(':'))
+    return tuple(url[len('http://lyrics.wikia.com/'):].rsplit(':', 1))
 
 def edit_url_from(url):
     """Get URL to "edit this page" from a given lyrics page"""
@@ -49,10 +56,13 @@ def lyrics(artist, song):
     """Get lyrics using the API and an edit page"""
     api_url = url_from_api(artist, song)
     artist_song = artist_song_from_api_url(api_url)
-    return lyrics_from('http://lyrics.wikia.com/', 'index.php', title='{0}:{1}'.format(*artist_song), action='edit')
+    return lyrics_from('http://lyrics.wikia.com/', 'index.php', title=u'{0}:{1}'.format(*artist_song), action='edit')
+
+print >>sys.stderr, sys.argv
 
 try:
-    print lyrics(*sys.argv[1:])
+    return_value = lyrics(*sys.argv[1:])
+    print return_value.encode(charset)
 except ValueError, e:
     print >>sys.stderr, str(e)
     sys.exit(1)
